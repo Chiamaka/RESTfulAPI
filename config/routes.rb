97e6d14eb70eae47316f2710a5ba2689e45206b2
Chiamaka Nwolisa <chiamakanwolisa@gmail.com>
2016-03-21 23:44:10 +0000

    Add defaults format json to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :cribs
-    resources :owners
-    resources :tenants
-    resources :schools
+  	defaults format: 'json' do
+	    resources :cribs
+	    resources :owners
+	    resources :tenants
+	    resources :schools
+  	end
   end
   root 'crib#index'
 end
